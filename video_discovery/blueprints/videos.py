fc5c0dfda199bac7b098fc2ee1e736d82e2c227d
Nancy Chauhan <nancychn1@gmail.com>
2021-06-21 20:30:46 +0530

    Implement API for limit and offset

--- video_discovery/blueprints/videos.py
+++ video_discovery/blueprints/videos.py
@@ -1,4 +1,5 @@
-from flask import jsonify
+from dateutil.parser import isoparse
+from flask import jsonify, request
 from flask.blueprints import Blueprint
 from sqlalchemy import desc
 
@@ -7,12 +8,33 @@ from video_discovery.models.video import Video
 blueprint = Blueprint('videos', __name__)
 
 
+def validation_error(message):
+    return jsonify({
+        'message': 'Validation error',
+        'errors': message
+    }), 400
+
+
 @blueprint.route('', methods=['GET'])
 def list_all_videos():
-    """
-    A GET API which returns the stored video data in a paginated response sorted in descending order of published datetime.
-    """
-    data = Video.query.order_by(desc(Video.published_at)).limit(10).all()
+    try:
+        offset_param = request.args.get('offset')
+        offset = isoparse(offset_param) if offset_param else None
+    except ValueError:
+        return validation_error('"offset" is not a valid date')
+
+    try:
+        limit = int(request.args.get('limit', default="10"))
+    except ValueError:
+        return validation_error('"limit" is not a valid integer')
+
+    query = Video.query
+
+    if offset:
+        query.filter(Video.published_at <= offset)
+
+    data = query.order_by(desc(Video.published_at)).limit(limit).all()
+
     return jsonify(serialize_many(data))
 
 
